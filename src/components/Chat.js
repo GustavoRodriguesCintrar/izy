fe5402ef1233d4a1fd6a097c545910c14e4ace84
Hian Motta <hian.hsm@gmail.com>
2023-08-09 11:29:12 -0300

    front-end bomba

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,38 +1,56 @@
 import React, { useState, useCallback } from 'react'
-import FormText from 'react-bootstrap/FormText'
-// import { postQuestion } from '../Services';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { postQuestion } from '../Services';
+import '../styles/message.css'
+
 
 export function Chat() {
     const [question, setQuestion] = useState('');
     const [chat, setChat] = useState([]);
     
-    const sendQuestion = useCallback(() => {
-        // const response = await postQuestion("/", {content: question})
-        const response = "resposta bem elaborada do chatgpt"
+    const sendQuestion = useCallback(async() => {
+        const response = await postQuestion("/", {user_message: question})
         setChat([...chat, question, response])
         setQuestion('')
-    })
+    }, [question, chat])
 
     return (
-        <div>
+        <div className='container'>
             <div>
                 {
                 chat.length === 0 ? 
-                (<p>Ainda não foi feita nenhuma pergunta</p>)
+                (
+                <p>Ainda não foi feita nenhuma pergunta</p>
+                )
                 :
                 chat.map((m, index) => 
-                (<p className={index%2 === 1 ? "response" : "question"}>{m}</p>))
+                (
+                <div className={index%2 === 1 ? "message-blue" : "message-orange"}>
+                    <p className="message-content">{m}</p>
+                </div>)
+                 )
                 }
             </div>
-            <div>
-                <input
-                type="text"
+            <InputGroup className="mb-3">
+                <Form.Control
+                placeholder="Escreva sua pergunta..."
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
                 value={ question }
                 onChange={ ({ target: { value } }) => setQuestion(value) }
-                placeholder="Escreva sua pergunta..."
+                onKeyPress={(event) => { if (event.key === "Enter") {sendQuestion()}}}
                 />
-                <button type='button' disabled={!(question.length > 1)} onClick={() => sendQuestion()}>Enviar</button>
-            </div>
+                <Button 
+                variant="outline-secondary" 
+                id="button-addon2"
+                disabled={!(question.length > 1)}
+                onClick={() => sendQuestion()}
+                >
+                Enviar
+                </Button>
+            </InputGroup>
         </div>
     )
 }
